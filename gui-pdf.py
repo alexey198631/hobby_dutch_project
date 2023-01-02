881e5167450947d2295b7188be1c2a9b4d182a32
Alex <al.an.gukov@gmail.com>
2023-01-02 17:03:15 +0100

    workable v1

--- gui-pdf.py
+++ gui-pdf.py
@@ -6,12 +6,14 @@ additional goal is to test tkinter the first time
 
 import tkinter as tk
 import PyPDF2
+from PyPDF2 import PdfReader
 from PIL import Image, ImageTk
+from tkinter.filedialog import askopenfile
 
 
 root = tk.Tk()
 
-canvas = tk.Canvas(root, width=600, height=300)
+canvas = tk.Canvas(root, width=300, height=100)
 canvas.grid(columnspan=3)
 
 #logo
@@ -25,6 +27,29 @@ logo_label.grid(column=1,row=0)
 instrusction = tk.Label(root, text='Select a PDF file', font='Arial')
 instrusction.grid(columnspan=3, column=0, row=1)
 
+def open_file():
+    browse_text.set('loading...')
+    file = askopenfile(parent=root, mode='rb', title="Choose a file",filetypes=[('PDf Files', '*.pdf')])
+    if file:
+
+        reader = PdfReader(file)
+        number_of_pages = len(reader.pages)
+        page = reader.pages[500]
+        text = page.extract_text()
+
+        #textbox
+        text_box = tk.Text(root,height=10, width=50, padx=15, pady=15)
+        text_box.insert(1.0, text)
+        text_box.tag_configure('center', justify='center')
+        text_box.tag_add('center',1.0,'end')
+        text_box.grid(column=1,row=3)
+
+        browse_text.set("Browse")
+
 #browse button
+browse_text = tk.StringVar()
+brows_btn = tk.Button(root, textvariable=browse_text, command=lambda: open_file(), font="Arial", height=2, width=10)
+browse_text.set("Browse")
+brows_btn.grid(column=1,row=2)
 
 root.mainloop()
